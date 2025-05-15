a2f54f8cd36ba3021dcd943fb38dd3b755d94ded
billmeiss <stefangazzana@gmail.com>
2025-05-15 19:47:57 +1000

    cheeky

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getCompanyESGReports() {
+    console.log(process.env.GEMINI_API_KEY);
     return this.appService.processCompanyReports({ withChunking: true });
   }
 
