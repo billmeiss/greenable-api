4edbc9c9bab61cdd3919775127884e5db134f959
billmeiss <stefangazzana@gmail.com>
2025-06-21 02:08:28 +1000

    s

--- src/app.service.ts
+++ src/app.service.ts
@@ -283,7 +283,7 @@ export class AppService {
   }
 
   async checkExistingReports(): Promise<any> {
-    const companies = await this.companyService.getExistingCompaniesFromSheet({ fromRow: 4941 });
+    const companies = await this.companyService.getExistingCompaniesFromSheet({ fromRow: 5425 });
     
     // Process companies in batches of 3
     const batchSize = 6;
